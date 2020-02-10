e0b41333708e7aa494c060ea4f58825e0564c846
Arseniy Khvorov <khvorov45@gmail.com>
2020-02-11 07:38:08 +1100

    removed token from tun_app

--- R/run_app.R
+++ R/run_app.R
@@ -1,15 +1,13 @@
 #' Run the Shiny Application
 #'
-#' @param token String or path to file containing the RedCap API token.
 #' @param ... Not used.
 #'
 #' @export
 #' @importFrom shiny shinyApp
 #' @importFrom golem with_golem_options
-run_app <- function(token, ...) {
-  token <- process_token(token)
+run_app <- function(...) {
   with_golem_options(
     app = shinyApp(ui = app_ui, server = app_server),
-    golem_opts = list(token = token, ...)
+    golem_opts = list(...)
   )
 }
