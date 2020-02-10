38a7a2b84b0526c0045b674e937c70c275c62c49
Arseniy Khvorov <khvorov45@gmail.com>
2020-02-11 09:41:45 +1100

    saving apipass key

--- data-raw/hashes.R
+++ data-raw/hashes.R
@@ -1,9 +1,11 @@
 ## code to prepare `DATASET` dataset goes here
 
+api_pass_key <- "hcwskrsrv!"
+
 create_hash_values <- function(...) {
   purrr::map_chr(
     list(...),
-    ~ openssl::sha256(hcwstudyapp:::process_apipass(.x), key = "hcwskrsrv!")
+    ~ openssl::sha256(hcwstudyapp:::process_apipass(.x), key = api_pass_key)
   )
 }
 
@@ -11,4 +13,7 @@ api_pass_hashes <- create_hash_values(
   "token", "data-raw/token-alt"
 )
 
-usethis::use_data(api_pass_hashes, internal = TRUE, overwrite = TRUE)
+usethis::use_data(
+  api_pass_hashes, api_pass_key,
+  internal = TRUE, overwrite = TRUE
+)
