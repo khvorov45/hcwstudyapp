3f17f4f15dd44ceccc4e3ed6ac7a5b34f992886c
Arseniy Khvorov <khvorov45@gmail.com>
2020-02-11 13:35:12 +1100

    added more data access functions

--- R/util_dataccess.R
+++ R/util_dataccess.R
@@ -9,3 +9,34 @@ get_redcap_data <- function(token, uri = "https://biredcap.mh.org.au/api/") {
     REDCapR::redcap_read_oneshot(uri, token, verbose = FALSE)$data
   )
 }
+
+#' Reformat variables
+#'
+#' @param raw Raw redcap data
+#'
+#' @importFrom rlang .data
+#'
+#' @export
+reformat_cols <- function(raw) {
+  raw %>%
+    dplyr::mutate(
+      record_id = as.integer(.data$record_id)
+    )
+}
+#' Convert to list
+#'
+#' Convert raw data to a list where each entry is an event
+#'
+#' @param raw Raw redcap data
+#'
+#' @export
+raw_to_list <- function(raw) {
+  lst <- raw %>%
+    dplyr::group_split(.data$redcap_event_name) %>%
+    purrr::map(~ dplyr::select_if(.x, function(vec) !all(is.na(vec))))
+  names(lst) <- purrr::map_chr(
+    lst, function(dat) unique(dat$redcap_event_name) %>%
+      stringr::str_replace("_arm_1", "")
+  )
+  lst %>% purrr::map(~ dplyr::select(.x, -redcap_event_name))
+}
--- tests/testthat/test-dataccess.R
+++ tests/testthat/test-dataccess.R
@@ -3,4 +3,11 @@ test_that("RedCap download works", {
   skip_if(tok == "", message = "REDAPITOK not set, skipping test")
   dat <- get_redcap_data(tok)
   expect_equal(class(dat), c("tbl_df", "tbl", "data.frame"))
+  dat_ref <- reformat_cols(dat)
+  expect_equal(names(dat_ref), names(dat))
+  dat_list <- raw_to_list(dat_ref)
+  expect_equal(
+    names(dat_list),
+    stringr::str_replace(unique(dat_ref$redcap_event_name), "_arm_1", "")
+  )
 })
