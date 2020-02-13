3073b368b74b4ab1db8caafafafff18b3f68bbb3
Arseniy Khvorov <khvorov45@gmail.com>
2020-02-14 09:07:37 +1100

    updatedata now updates on correct password

--- R/mod_updatedata.R
+++ R/mod_updatedata.R
@@ -11,17 +11,29 @@ ui_updatedata <- function(id = "updatedata", label = "Update data", colw = 3) {
 
 server_updatedata <- function(input, output, session, password_verified) {
   all_dat <- reactiveVal()
+
+  # Update on correct password update
+  observe({
+    if (is.null(password_verified())) return()
+    if (password_verified()) all_dat(update_data(output))
+  })
+
+  # Update on button press
   observeEvent(input$update, {
     if (is.null(password_verified())) return()
     if (!password_verified()) return()
-    all_dat(down_trans_redcap(golem::get_golem_options("token")))
-    output$time <- renderUI({
-      HTML(
-        glue::glue(
-          "<p>Last data update:<br/>{as.character(Sys.time())}</p>"
-        )
-      )
-    })
+    all_dat(update_data(output))
   })
   all_dat
+}
+
+update_data <- function(output) {
+  output$time <- renderUI({
+    HTML(
+      glue::glue(
+        "<p>Last data update:<br/>{as.character(Sys.time())}</p>"
+      )
+    )
+  })
+  down_trans_redcap(golem::get_golem_options("token"))
 }
