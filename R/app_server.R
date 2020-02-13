670b80103839277431ecd2d105c1c565bd10474b
Arseniy Khvorov <khvorov45@gmail.com>
2020-02-14 07:37:07 +1100

    removed unnecessary

--- R/app_server.R
+++ R/app_server.R
@@ -3,6 +3,4 @@ app_server <- function(input, output, session) {
   password_verified <- callModule(server_apipass, "apipass")
   all_data <- callModule(server_updatedata, "updatedata", password_verified)
   callModule(server_recruitvh, "recruitvh", password_verified, all_data)
-  #callModule(server_seltableout, "seltableout", password_verified, all_dat)
-  #callModule(server_seltable, "seltable", password_verified)
 }
