e48265d942e1d5abb2c73fdaf59ae0d645e36390
Arseniy Khvorov <khvorov45@gmail.com>
2020-02-18 11:44:16 +1100

    changed selectInput to shinyWidgets alternative

--- R/ui_siteselect.R
+++ R/ui_siteselect.R
@@ -2,13 +2,15 @@
 #' @noRd
 siteselect <- function(id, label = "Site", choices = list("All"),
                        selected = NULL, multiple = FALSE) {
-  selectInput(id, label, choices, selected, multiple)
+  shinyWidgets::pickerInput(id, label, choices, selected = "All", multiple)
 }
 
 #' Updates the above list, makes sure 'all' is persistent
 #' @noRd
 update_siteselect <- function(session, id, new_choices) {
-  updateSelectInput(session, id, choices = as.list(c("All", new_choices)))
+  shinyWidgets::updatePickerInput(
+    session, id, choices = as.list(c("All", new_choices))
+  )
 }
 
 #' Dynamically updates the above list on password or data change
