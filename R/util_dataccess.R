4fa646679f5590e98e173ab991a22db9acad4370
Arseniy Khvorov <khvorov45@gmail.com>
2020-03-02 12:26:31 +1100

    added site test dates to swab table

--- R/util_dataccess.R
+++ R/util_dataccess.R
@@ -115,7 +115,8 @@ get_tbl_symptom <- function(raw_consented) {
 #' @export
 get_tbl_swab <- function(raw_consented) {
   needed_cols <- c(
-    "record_id", "swab_collection", "samp_date", "survey_week"
+    "record_id", "swab_collection", "samp_date", "survey_week",
+    "site_rec_date", "site_test_date"
   )
   raw_consented %>%
     filter(.data$redcap_event_name == "infection") %>%
