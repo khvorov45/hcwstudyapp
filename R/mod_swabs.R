12d0757beb644c7dfadee2b5be576b1f186ee5ce
Arseniy Khvorov <khvorov45@gmail.com>
2020-03-02 12:12:32 +1100

    implemented orphan filter

--- R/mod_swabs.R
+++ R/mod_swabs.R
@@ -2,7 +2,15 @@
 #' @noRd
 ui_swabs <- function(id = "swabs", label = "Swabs") {
   ns <- NS(id)
-  tablepanel(ns, label)
+  tablepanel(
+    ns, label,
+    shinyWidgets::prettyCheckbox(ns("orphan"), "Orphan"),
+    HTML(
+      "Orphan swabs are swabs whose survey_week does not
+      correspond to any date_symptom_survey
+      <br/>"
+    )
+  )
 }
 
 #' Server for swabs
@@ -17,7 +25,22 @@ server_swabs <- function(input, output, session, dat) {
       select("record_id", "pid", "site_name", everything())
   })
   update_tablepanel_dyn(session, tbl)
-  tbl_new <- update_tbl_dyn(input, tbl)
+
+  tbl_orphan <- reactive({
+    tbl_current <- tbl()
+    if (input$orphan) {
+      left_join(tbl_current, dat()$symptom, "record_id") %>%
+        group_by(.data$record_id) %>%
+        filter(!.data$survey_week %in% .data$date_symptom_survey) %>%
+        ungroup() %>%
+        select("record_id", "samp_date", "survey_week") %>%
+        unique() %>%
+        inner_join(tbl_current, c("record_id", "samp_date", "survey_week")) %>%
+        select("record_id", "pid", "site_name", everything())
+    } else tbl_current
+  })
+
+  tbl_new <- update_tbl_dyn(input, tbl_orphan)
   render_tablepanel_table(output, tbl_new)
   output$download <- download_data(output, "swab", tbl_new)
 }
