f61e2d1311242e76f3c2a464d1d9196aeb1c4cf1
Arseniy Khvorov <khvorov45@gmail.com>
2020-03-20 11:36:01 +1100

    moved swabs to module

--- R/mod_swabs.R
+++ R/mod_swabs.R
@@ -2,13 +2,15 @@
 #' @noRd
 ui_swabs <- function(id = "swabs", label = "Swabs") {
   ns <- NS(id)
-  tablepanel(
-    ns, label,
-    shinyWidgets::prettyCheckbox(ns("orphan"), "Orphan"),
-    HTML(
-      "Orphan swabs are swabs whose survey_week does not
-      correspond to any date_symptom_survey
-      <br/>"
+  ui_tablepanel(
+    ns("tablepanel"), label,
+    data_ui = list(
+      shinyWidgets::prettyCheckbox(ns("orphan"), "Orphan"),
+      HTML(
+        "Orphan swabs are swabs whose survey_week does not
+        correspond to any date_symptom_survey
+        <br/>"
+      )
     )
   )
 }
@@ -24,7 +26,6 @@ server_swabs <- function(input, output, session, dat) {
     inner_join(all_dat$swab, all_dat$participant_essential, "record_id") %>%
       select("record_id", "pid", "site_name", everything())
   })
-  update_tablepanel_dyn(session, tbl)
 
   tbl_orphan <- reactive({
     tbl_current <- tbl()
@@ -42,7 +43,5 @@ server_swabs <- function(input, output, session, dat) {
     }
   })
 
-  tbl_new <- update_tbl_dyn(input, tbl_orphan)
-  render_tablepanel_table(output, tbl_new)
-  output$download <- download_data(output, "swab", tbl_new)
+  callModule(server_tablepanel, "tablepanel", tbl_orphan, "swabs")
 }
