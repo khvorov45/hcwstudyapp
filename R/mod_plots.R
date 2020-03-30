27b5569da744c5ebff007c8a1207ed097c3e60b2
Arseniy Khvorov <khvorov45@gmail.com>
2020-03-30 14:42:53 +1100

    modularised questionnaire completeness

--- R/mod_plots.R
+++ R/mod_plots.R
@@ -5,7 +5,8 @@ ui_plots <- function(id = "plots", label = "Plots") {
     fluidRow(
       column(4, ui_binfilt(
         ns("binfilt-addb"), "Consent to additional bleed", "add_bleed"
-      ))
+      )),
+      column(4, ui_bqcomp(ns("bqcomp")))
     ),
     fluidRow(
       column(4, sliderInput(ns("fontsize"), "Font size", 10, 30, 20))
@@ -24,6 +25,7 @@ server_plots <- function(input, output, session, data, dark) {
   tbl_part_addb <- callModule(
     server_binfilt, "binfilt-addb", tbl_part, "add_bleed"
   )
+  tbl_part_bq <- callModule(server_bqcomp, "bqcomp", tbl_part)
   output$histvachx <- renderPlot(
     plot_vachx(tbl_part_addb(), input$fontsize, dark())
   )
--- R/mod_raw_participant.R
+++ R/mod_raw_participant.R
@@ -6,11 +6,7 @@ ui_raw_participant <- function(id, label) {
       column(3, ui_binfilt(
         ns("binfilt-addb"), "Consent to additional bleed", "add_bleed"
       )),
-      column(4, shinyWidgets::radioGroupButtons(
-        ns("qmiss"), "Questionnaire completeness",
-        list("Any", "Complete", "Incomplete"),
-        direction = "horizontal"
-      ))
+      column(4, ui_bqcomp(ns("bqcomp")))
     )
   )
 }
@@ -20,31 +16,7 @@ server_raw_participant <- function(input, output, session, data) {
   tbl_part_addb <- callModule(
     server_binfilt, "binfilt-addb", tbl_participant, "add_bleed"
   )
-  tbl_part_qmiss <- reactive({
-    tbl <- tbl_part_addb()
-    if (input$qmiss == "Any") {
-      return(tbl)
-    }
-    complete_ids <- tbl %>%
-      select(
-        -"pid", -"site_name", -"mobile_number", -"email",
-        -"c3_spec", -"c4_spec"
-      ) %>%
-      mutate_all(as.character) %>%
-      tidyr::pivot_longer(
-        -"record_id",
-        names_to = "question", values_to = "response"
-      ) %>%
-      group_by(.data$record_id) %>%
-      summarise(complete_status = all(!is.na(.data$response))) %>%
-      filter(.data$complete_status) %>%
-      pull(.data$record_id)
-    if (input$qmiss == "Complete") {
-      filter(tbl, .data$record_id %in% complete_ids)
-    } else {
-      filter(tbl, !.data$record_id %in% complete_ids)
-    }
-  })
+  tbl_part_qmiss <- callModule(server_bqcomp, "bqcomp", tbl_part_addb)
   callModule(
     server_raw_table, "participant-raw-table", tbl_part_qmiss, "participant"
   )
