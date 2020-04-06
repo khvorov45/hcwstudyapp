ba1122e36374bdcb697935cf37e8eff390e9b79a
Arseniy Khvorov <khvorov45@gmail.com>
2020-04-06 11:31:20 +1000

    removed DT server-side processing [ci skip]

--- R/table_render.R
+++ R/table_render.R
@@ -2,6 +2,7 @@ table_render <- function(tbl, vars = NULL, pagelength = 100) {
   if (!is.null(vars)) tbl <- tbl[vars]
   DT::renderDataTable(
     tbl,
+    server = FALSE,
     style = "bootstrap4",
     rownames = FALSE,
     options = list(
