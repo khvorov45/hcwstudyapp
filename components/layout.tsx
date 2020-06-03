ca0c17cd334ae18a956018f7a1617f6e12634560
Arseniy Khvorov <khvorov45@gmail.com>
2020-06-04 09:58:10 +1000

    modularised theme

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,3 +1,5 @@
+import { ThemeSwitch } from './theme'
+
 export default function Layout ({
   children
 }: {
@@ -9,22 +11,9 @@ export default function Layout ({
         <div id="title">
           <h1>Study reports</h1>
         </div>
-        <div id="themeswitch">
-          <i onClick={changeTheme} className="material-icons">invert_colors</i>
-        </div>
+        <ThemeSwitch />
       </nav>
       <main>{children}</main>
     </>
   )
-}
-
-function changeTheme () {
-  let theme: string
-  if (document.documentElement.getAttribute('theme') === 'dark') {
-    theme = 'light'
-  } else {
-    theme = 'dark'
-  }
-  localStorage.setItem('theme', theme)
-  document.documentElement.setAttribute('theme', theme)
 }
