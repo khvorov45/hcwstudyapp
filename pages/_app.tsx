b600e8ff07522d6c15f95f5b790e820ff58a14b6
Arseniy Khvorov <khvorov45@gmail.com>
2020-06-03 14:41:11 +1000

    add global css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import { AppProps } from 'next/app'
 import '../styles/material-icons.css'
+import '../styles/global.css'
 
 export default function App ({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
