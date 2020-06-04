8444f5d9285b0d5f2c53938643393d83a222cc13
Arseniy Khvorov <khvorov45@gmail.com>
2020-06-04 12:02:31 +1000

    a way to access server

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,24 @@
 import Layout from '../components/layout'
 import InfoMessage from '../components/info'
+import { getPasswords } from '../lib/passwords'
 
-export default function Home () {
+export default function Home (props: {authorised: boolean}) {
+  console.log(props)
   return (
     <Layout>
-      <InfoMessage content="Please use the given link to access reports" />
+      {
+        props.authorised
+          ? <InfoMessage content="Supposed to show reports" />
+          : <InfoMessage
+            content="Please use the given link to access reports"
+          />
+      }
     </Layout>
   )
+}
+
+export async function getServerSideProps (context) {
+  console.log(context.query)
+  console.log(getPasswords())
+  return { props: { authorised: false } }
 }
