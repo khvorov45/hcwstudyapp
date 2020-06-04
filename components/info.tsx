cdaabfb70092fddab12584d596152cf6274d5c28
Arseniy Khvorov <khvorov45@gmail.com>
2020-06-04 10:51:11 +1000

    modularised info css

--- components/info.tsx
+++ components/info.tsx
@@ -1,3 +1,5 @@
+import styles from './info.module.css'
+
 export default function InfoMessage (props: {content: string}) {
-  return <p className="info">{props.content}</p>
+  return <p className={styles.info}>{props.content}</p>
 }
