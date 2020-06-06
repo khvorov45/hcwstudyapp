5a3a903deb88f08af984a4499325769b75cccc0d
Arseniy Khvorov <khvorov45@gmail.com>
2020-06-06 16:21:46 +1000

    generate link

--- pages/api/sendemail.ts
+++ pages/api/sendemail.ts
@@ -1,16 +1,29 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 import db from '../../lib/db'
+import cryptoRandomString from 'crypto-random-string'
+import bcrypt from 'bcrypt'
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   const allUsers = await db.user.getUsers()
   for (const user of allUsers) {
     if (req.body.email !== user.email) continue
-    console.log(user)
+    const token = cryptoRandomString({ length: 10 })
+    const link = generateLink(
+      req.headers.origin, token, user.id.toString()
+    )
+    console.log(`Supposed to send ${link} to ${user.email}`)
     res.status(200).end()
     return
   }
   // TODO
-  // Generate a token and send it
-  // Hash the token and store it in the database
+  // Send the token
+  // Hash the token
+  // Store token in the database
   res.status(404).end()
+}
+
+function generateLink (
+  origin: string | string[], token: string, id: string
+): string {
+  return `${origin}/?id=${id}&token=${token}`
 }
