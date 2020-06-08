cf207875fffbeaf83cdf93aee84e22cadd2d863c
Arseniy Khvorov <khvorov45@gmail.com>
2020-06-09 09:56:59 +1000

    authorisation process

--- lib/authorise.ts
+++ lib/authorise.ts
@@ -1,8 +1,12 @@
 import db from './db'
+import bcrypt from 'bcrypt'
 
-export async function authorise (site?: string, token?: string) {
-  db.temp()
-  if (!site || !token) return false
-  console.log(`supposed to authorise access to ${site} with ${token}`)
+export async function authorise (id: number, token: string) {
+  if (!id || !token) return null
+  const allUsers = await db.user.getUsers()
+  for (const user of allUsers) {
+    if (user.id !== id) continue
+    return await bcrypt.compare(token, user.tokenhash)
+  }
   return false
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,21 +2,53 @@ import Layout from '../components/layout'
 import InfoMessage from '../components/info'
 import EmailForm from '../components/emailform'
 import Head from 'next/head'
+import { authorise } from '../lib/authorise'
 
-export default function Home () {
+function Noquery (props: {info: string, message: string}) {
+  return (
+    <>
+      <InfoMessage
+        content={props.info}
+      />
+      <EmailForm
+        message={props.message}
+      />
+    </>
+  )
+}
+
+export default function Home (props: {authorised: boolean}) {
+  var PageElement: JSX.Element
+  if (props.authorised === null) {
+    PageElement = <Noquery
+      info="Please use the given link to access reports"
+      message="If you don't have a link, enter your email below and a new one
+        will be sent to you"
+    />
+  } else if (props.authorised) {
+    PageElement = <p>Authorised</p>
+  } else {
+    PageElement = <Noquery
+      info="Link is not valid"
+      message="Enter your email below and a new one
+        will be sent to you"
+    />
+  }
   return (
     <Layout>
       <Head>
         <title>HCW flu study reports</title>
         <meta name="Description" content="Reports for the HCW flu study" />
       </Head>
-      <InfoMessage
-        content="Please use the given link to access reports"
-      />
-      <EmailForm
-        message="If you don't have a link, enter your email below and a new one
-        will be sent to you"
-      />
+      {PageElement}
     </Layout>
   )
+}
+
+export async function getServerSideProps (context) {
+  return {
+    props: {
+      authorised: await authorise(+context.query.id, context.query.token)
+    }
+  }
 }
