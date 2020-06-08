bf8e6218a6421c7b8ac8a7ea9af6f3c846434108
Arseniy Khvorov <khvorov45@gmail.com>
2020-06-09 08:36:26 +1000

    add withdrawn selected to withdrawal filter

--- R/mod_withdrawn.R
+++ R/mod_withdrawn.R
@@ -2,7 +2,7 @@ ui_withdrawn <- function(id, label = "Withdrawal status") {
   ns <- NS(id)
   shinyWidgets::radioGroupButtons(
     ns("withdrawn"), label,
-    list("Any", "Not withdrawn"),
+    list("Any", "Not withdrawn", "Withdrawn"),
     direction = "horizontal",
     justified = FALSE,
     selected = c("Not withdrawn")
@@ -16,8 +16,10 @@ server_withdrawn <- function(input, output, session, all_dat, tbl_name) {
     dat <- all_data[[tbl_name]]
     if (input$withdrawn == "Any") {
       dat
-    } else {
+    } else if (input$withdrawn == "Not withdrawn") {
       filter(dat, !.data$record_id %in% all_data$withdrawal$record_id)
+    } else {
+      filter(dat, .data$record_id %in% all_data$withdrawal$record_id)
     }
   })
 }
