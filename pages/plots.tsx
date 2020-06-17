e155f48780561ff15b10766ce24144d9765ff5af
Arseniy Khvorov <khvorov45@gmail.com>
2020-06-17 10:16:54 +1000

    add a placeholder plot

--- pages/plots.tsx
+++ pages/plots.tsx
@@ -1,10 +1,15 @@
-import Layout from '../components/layout'
 import Head from 'next/head'
+import { LineChart, Line, CartesianGrid, XAxis, YAxis } from 'recharts'
+import Layout from '../components/layout'
 import { authorise } from '../lib/authorise'
 
 export default function Plots (
   props: {authorised: boolean, id: number, token: string}
 ) {
+  const data = [
+    { name: 'Page A', uv: 400, pv: 2400, amt: 2400 },
+    { name: 'Page B', uv: 500, pv: 2400, amt: 2400 }
+  ]
   return (
     <Layout
       id={props.id}
@@ -16,7 +21,12 @@ export default function Plots (
         <title>Plots - HCW flu study</title>
         <meta name="Description" content="Plots - HCW flu study" />
       </Head>
-      <p>Plots here</p>
+      <LineChart width={400} height={400} data={data}>
+        <Line type="monotone" dataKey="uv" stroke="#8884d8" />
+        <CartesianGrid stroke="#ccc" />
+        <XAxis dataKey="name" />
+        <YAxis />
+      </LineChart>
     </Layout>
   )
 }
