7dcf3c254db1b396882abb88f267728c2f6a57e6
Arseniy Khvorov <khvorov45@gmail.com>
2020-06-17 11:27:13 +1000

    api endpoint for participants

---
+++ pages/api/getparticipants.ts
@@ -0,0 +1,13 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import db from '../../lib/db'
+import { authorise } from '../../lib/authorise'
+
+export default async function (req: NextApiRequest, res: NextApiResponse) {
+  const authorised = await authorise(+req.body.id, req.body.token)
+  if (!authorised) {
+    res.status(401).end()
+    return
+  }
+  const accessGroup = (await (await db).getUser(+req.body.id)).accessGroup
+  res.status(200).send(await (await db).getParticipants(accessGroup))
+}
