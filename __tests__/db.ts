269e89dbceb8c061d18c2764ef7d743008742610
Arseniy Khvorov <khvorov45@gmail.com>
2020-06-19 15:10:43 +1000

    put tests together
    Easier cleanup

--- __tests__/db.ts
+++ __tests__/db.ts
@@ -58,46 +58,44 @@ test('Participant export by access group', async () => {
   }
 })
 
-test('AccessGroup update', async () => {
-  if (await fs.existsSync(userTestDbPath)) fs.unlinkSync(userTestDbPath)
-  let extraAccessGroups = ['test-access-group', 'temp-access-group']
-  const db = await new UserDB(
-    userTestDbPath, userSql, undefined,
-    async () => (await config.getExtraAccessGroups())
-      .concat(extraAccessGroups)
-  ).init()
-  const accesGroupsBefore = await db.getAccessGroups()
-  expect(accesGroupsBefore.includes('test-access-group')).toBe(true)
-  expect(accesGroupsBefore.includes('temp-access-group')).toBe(true)
-  extraAccessGroups = ['test-access-group', 'temp2-access-group']
-  await db.update()
-  const accessGroupsAfter = await db.getAccessGroups()
-  expect(accessGroupsAfter.includes('test-access-group')).toBe(true)
-  expect(accessGroupsAfter.includes('temp-access-group')).toBe(false)
-  expect(accessGroupsAfter.includes('temp2-access-group')).toBe(true)
-  fs.unlinkSync(userTestDbPath)
-})
-
-test('User update', async () => {
+test('Update', async () => {
   if (fs.existsSync(userTestDbPath)) fs.unlinkSync(userTestDbPath)
+
+  let extraAccessGroups = ['test-access-group', 'temp-access-group']
   let extraUsers = [
     { email: 'test@test.test', accessGroup: 'unrestricted' },
     { email: 'test-persist@test.test', accessGroup: 'unrestricted' }
   ]
+
   const db = await new UserDB(
-    userTestDbPath, userSql, async () => extraUsers
+    userTestDbPath,
+    userSql,
+    async () => extraUsers,
+    async () => (await config.getExtraAccessGroups()).concat(extraAccessGroups)
   ).init()
+
+  const accesGroupsBefore = await db.getAccessGroups()
+  expect(accesGroupsBefore.includes('test-access-group')).toBe(true)
+  expect(accesGroupsBefore.includes('temp-access-group')).toBe(true)
   const allEmailsBefore = await db.getUserEmails()
   expect(allEmailsBefore.includes('test@test.test')).toBe(true)
   expect(allEmailsBefore.includes('test-persist@test.test')).toBe(true)
   expect((await db.getUser('email', 'test-persist@test.test')).accessGroup)
     .toBe('unrestricted')
+
+  extraAccessGroups = ['test-access-group', 'temp2-access-group']
   extraUsers = [
     { email: 'test2@test.test', accessGroup: 'unrestricted' },
     { email: 'test-persist@test.test', accessGroup: 'admin' },
     { email: 'arseniy.khvorov@mh.org.au', accessGroup: 'unrestricted' }
   ]
-  await db.updateUsers()
+
+  await db.update()
+
+  const accessGroupsAfter = await db.getAccessGroups()
+  expect(accessGroupsAfter.includes('test-access-group')).toBe(true)
+  expect(accessGroupsAfter.includes('temp-access-group')).toBe(false)
+  expect(accessGroupsAfter.includes('temp2-access-group')).toBe(true)
   const allEmailsAfter = await db.getUserEmails()
   expect(allEmailsAfter.includes('test@test.test')).toBe(false)
   expect(allEmailsAfter.includes('test2@test.test')).toBe(true)
@@ -107,11 +105,14 @@ test('User update', async () => {
     .toBe('admin')
   expect((await db.getUser('email', 'arseniy.khvorov@mh.org.au')).accessGroup)
     .toBe('unrestricted')
+
   extraUsers = [
     { email: 'arseniy.khvorov@mh.org.au', accessGroup: 'admin' }
   ]
-  await db.updateUsers()
+
+  await db.update()
+
   expect((await db.getUser('email', 'arseniy.khvorov@mh.org.au')).accessGroup)
     .toBe('admin')
   fs.unlinkSync(userTestDbPath)
-})
+}, 15000)
