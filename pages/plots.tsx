d57da73b3e4b9f94b706c9bcf8307b0edcec3ad6
Arseniy Khvorov <khvorov45@gmail.com>
2020-06-24 12:40:25 +1000

    update packages

--- pages/plots.tsx
+++ pages/plots.tsx
@@ -1,15 +1,10 @@
 import Head from 'next/head'
-import { LineChart, Line, CartesianGrid, XAxis, YAxis } from 'recharts'
 import Layout from '../components/layout'
 import { authorise } from '../lib/authorise'
 
 export default function Plots (
   props: {authorised: boolean, email: string, token: string}
 ) {
-  const data = [
-    { name: 'Page A', uv: 400, pv: 2400, amt: 2400 },
-    { name: 'Page B', uv: 500, pv: 2400, amt: 2400 }
-  ]
   return (
     <Layout
       authorised={props.authorised}
@@ -21,12 +16,7 @@ export default function Plots (
         <title>Plots - HCW flu study</title>
         <meta name="Description" content="Plots - HCW flu study" />
       </Head>
-      <LineChart width={400} height={400} data={data}>
-        <Line type="monotone" dataKey="uv" stroke="#8884d8" />
-        <CartesianGrid stroke="#ccc" />
-        <XAxis dataKey="name" />
-        <YAxis />
-      </LineChart>
+      Plot here
     </Layout>
   )
 }
