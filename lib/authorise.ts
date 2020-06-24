dd551034367da1fa95908dafd19bfc91be2a9c58
Arseniy Khvorov <khvorov45@gmail.com>
2020-06-24 12:17:27 +1000

    fixed authorise with no hash

--- lib/authorise.ts
+++ lib/authorise.ts
@@ -4,6 +4,6 @@ import bcrypt from 'bcrypt'
 export async function authorise (email: string, token: string) {
   if (!email || !token) return null
   const user = await (await db).getUser(email)
-  if (!user) return false
+  if (!user || !user.tokenhash) return false
   return await bcrypt.compare(token, user.tokenhash)
 }
