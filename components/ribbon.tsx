590c747c278902bbc4a51bb0d2fc727f96219908
Arseniy Khvorov <khvorov45@gmail.com>
2020-06-24 11:44:26 +1000

    shortened update handling

--- components/ribbon.tsx
+++ components/ribbon.tsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react'
 import { trackPromise } from 'react-promise-tracker'
 import { ButtonWithTimestamp } from './input'
-import { fetchOwnApi, accessAPI } from '../lib/util'
+import { accessAPI } from '../lib/util'
 import styles from './ribbon.module.css'
 
 export default function Ribbon (
@@ -19,30 +19,27 @@ export function UpdateDatabaseButton (
   { email, token, afterdbUpdate }:
   {email: string, token: string, afterdbUpdate: () => Promise<void>}
 ) {
-  async function updateDB () {
+  async function updateDB (actuallyThough: boolean) {
     async function updateAndAfter () {
-      const date = await fetchOwnApi(email, token, 'update')
+      let date: Date
+      if (actuallyThough) {
+        date = await accessAPI(
+          'update', 'POST', { email: email, token: token }
+        )
+      } else {
+        date = await accessAPI('update', 'GET')
+      }
       await afterdbUpdate()
       setLastUpdate(new Date(date))
     }
     await trackPromise(updateAndAfter(), 'updatedb')
   }
-  // Don't want to update the database on every mount, just fetch when
-  // it was last updated
-  async function dontUpdateDB () {
-    async function dontUpdateAndAfter () {
-      const date = await accessAPI('update', 'GET')
-      await afterdbUpdate()
-      setLastUpdate(new Date(date))
-    }
-    await trackPromise(dontUpdateAndAfter(), 'updatedb')
-  }
-  useEffect(() => { dontUpdateDB() }, [])
+  useEffect(() => { updateDB(false) }, [])
   const [lastUpdate, setLastUpdate] = useState(new Date(0))
   return <ButtonWithTimestamp
     label="Update"
     timestamp={lastUpdate}
-    onClick={updateDB}
+    onClick={() => updateDB(true)}
     promiseArea='updatedb'
   />
 }
