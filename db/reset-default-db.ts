beda187629536d44a12559a770fc1c64af3b138b
Arseniy Khvorov <khvorov45@gmail.com>
2020-06-27 14:09:55 +1000

    Update reset-default-db.ts

--- db/reset-default-db.ts
+++ db/reset-default-db.ts
@@ -2,7 +2,7 @@ import { Postgres } from '../lib/db'
 
 test('reset-default-db', async () => {
   // Default initialisation (check that it doesn't fail but don't modify)
-  const db = await new Postgres().init()
+  const db = new Postgres()
   await db.update(true)
   expect(await db.isEmpty()).toBe(false)
   await db.end()
