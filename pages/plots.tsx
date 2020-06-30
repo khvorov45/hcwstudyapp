eb28667da5527e07168a49e1c97d5139f675fd30
Arseniy Khvorov <khvorov45@gmail.com>
2020-06-30 11:29:19 +1000

    add histogram

--- pages/plots.tsx
+++ pages/plots.tsx
@@ -1,15 +1,39 @@
 import { useState } from 'react'
 import Head from 'next/head'
+import { bin } from 'd3-array'
+import {
+  BarChart, CartesianGrid, XAxis, YAxis, Tooltip, Legend, Bar
+} from 'recharts'
 import Layout from '../components/layout'
 import db from '../lib/db'
 import { accessAPI } from '../lib/util'
 import Ribbon from '../components/ribbon'
 
+function Histogram ({ data, x }: {data: any, x: string}) {
+  const xvec = data.map(row => row[x])
+  const histData = bin()(xvec).reduce(
+    (acc, el) => {
+      acc.push({ x: (el.x0 + el.x1) / 2, y: el.length })
+      return acc
+    },
+    []
+  )
+  console.log(histData)
+  return <BarChart width={730} height={250} data={histData}>
+    <CartesianGrid strokeDasharray="3 3" />
+    <XAxis dataKey="x" />
+    <YAxis />
+    <Tooltip />
+    <Legend />
+    <Bar dataKey="y" fill="#8884d8" />
+  </BarChart>
+}
+
 export default function Plots (
   { authorised, email, token } :
   {authorised: boolean, email: string, token: string}
 ) {
-  const [_data, setData] = useState([])
+  const [data, setData] = useState([])
   async function updateData () {
     setData(await accessAPI(
       'getparticipants', 'GET',
@@ -34,7 +58,7 @@ export default function Plots (
         afterdbUpdate={updateData}
         elements={{}}
       />
-      Plot here
+      <Histogram data={data} x='age'/>
     </Layout>
   )
 }
