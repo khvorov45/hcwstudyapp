965089cac137663c0502fd4ff238f2fc591f3db5
Arseniy Khvorov <khvorov45@gmail.com>
2020-07-03 12:21:13 +1000

    add api endpoint for user authorisation

---
+++ pages/api/authoriseuser.ts
@@ -0,0 +1,19 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import db from '../../lib/db'
+
+export default async function (req: NextApiRequest, res: NextApiResponse) {
+  if (req.method !== 'GET') {
+    res.setHeader('Allow', 'GET')
+    res.status(405).end()
+    return
+  }
+  res.setHeader('Content-Type', 'application/json')
+  if (!req.query.email || !req.query.token) {
+    res.status(200).send(JSON.stringify(false))
+    return
+  }
+  const email = req.query.email.toString().toLowerCase()
+  const token = req.query.token.toString()
+  res.setHeader('Content-Type', 'application/json')
+  res.status(200).send(JSON.stringify(await db.authoriseUser(email, token)))
+}
