3b7c2b5c9e3121d3fa7f604dded10298abe7df92
Arseniy Khvorov <khvorov45@gmail.com>
2020-07-03 14:40:40 +1000

    removed unnecessary bits from layout

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,43 +1,5 @@
-import Head from 'next/head'
 import Navbar from './navbar'
-import InfoMessage from './info'
-import EmailForm from './emailform'
 import { User } from '../lib/util'
-
-function EmptyCred () {
-  return <>
-    <Head>
-      <title>HCW flu study reports</title>
-      <meta name="Description" content="Reports for the HCW flu study" />
-    </Head>
-    <InfoMessage
-      content="Please use the given link to access reports"
-    />
-    <EmailForm
-      message="If you don't have a link, enter your email below and a new one
-        will be sent to you"
-    />
-  </>
-}
-
-function Unauthorised () {
-  return <>
-    <Head>
-      <title>HCW flu study reports - unautorised</title>
-      <meta
-        name="Description"
-        content="Reports for the HCW flu study - unautorised access"
-      />
-    </Head>
-    <InfoMessage
-      content="Link is not valid"
-    />
-    <EmailForm
-      message="Enter your email below and a new one
-      will be sent to you"
-    />
-  </>
-}
 
 export default function Layout (
   { children, user, active }: {
@@ -46,19 +8,12 @@ export default function Layout (
     active: string,
   }
 ) {
-  var pageContent = children
-  if (user.authorised === null) {
-    pageContent = EmptyCred()
-  } else if (!user.authorised) {
-    pageContent = Unauthorised()
-  }
-  return (
-    <>
-      <Navbar
-        user={user}
-        active={active}
-      />
-      <main>{pageContent}</main>
-    </>
-  )
+  if (!user.authorised) return <></>
+  return <>
+    <Navbar
+      user={user}
+      active={active}
+    />
+    <main>{children}</main>
+  </>
 }
