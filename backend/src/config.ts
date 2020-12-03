c2083f25bc8d082e5b4438c16e83f6e3d0b3abc0
Arseniy Khvorov <khvorov45@gmail.com>
2020-12-03 11:01:21 +1100

    add db connection string to config

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -1,3 +1,7 @@
 export const BACKEND_PORT = process.env.HFS_BACKEND_PORT
   ? parseInt(process.env.HFS_BACKEND_PORT)
   : 8000
+
+export const DB_CONNECTION_STRING =
+  process.env.HFS_DB_CONNECTION_STRING ??
+  "postgres://postgres:admin@localhost:7000/postgres"
