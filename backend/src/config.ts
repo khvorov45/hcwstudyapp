6cf564fdd4be223ec82ed22cfc39860d562ccc5e
Arseniy Khvorov <khvorov45@gmail.com>
2020-12-03 13:07:38 +1100

    add db clean env variable for config

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -5,3 +5,5 @@ export const BACKEND_PORT = process.env.HFS_BACKEND_PORT
 export const DB_CONNECTION_STRING =
   process.env.HFS_DB_CONNECTION_STRING ??
   "postgres://postgres:admin@localhost:7000/postgres"
+
+export const DB_CLEAN = process.env.HFS_DB_CLEAN !== undefined
