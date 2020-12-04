ebdb290bea301d9f2280535cfaaec36fe447f957
Arseniy Khvorov <khvorov45@gmail.com>
2020-12-04 12:39:43 +1100

    Create data.ts

---
+++ backend/src/data.ts
@@ -0,0 +1,19 @@
+import * as t from "io-ts"
+
+// https://github.com/gcanti/io-ts/blob/master/index.md#union-of-string-literals
+export const AccessGroupV = t.keyof({
+  admin: null,
+  unrestricted: null,
+  melbourne: null,
+})
+export type AccessGroup = t.TypeOf<typeof AccessGroupV>
+
+export const UserV = t.type({
+  email: t.string,
+  accessGroup: AccessGroupV,
+  tokenHash: t.string,
+})
+export type User = t.TypeOf<typeof UserV>
+
+export const FullDataV = t.type({ user: UserV })
+export type FullData = t.TypeOf<typeof FullDataV>
