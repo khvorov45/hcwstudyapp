401f769d5a81636019e9edd621df346ef583e953
Arseniy Khvorov <khvorov45@gmail.com>
2020-12-04 11:48:01 +1100

    Delete config.ts

--- backend/src/config.ts
+++
@@ -1,9 +0,0 @@
-export const BACKEND_PORT = process.env.HFS_BACKEND_PORT
-  ? parseInt(process.env.HFS_BACKEND_PORT)
-  : 8000
-
-export const DB_CONNECTION_STRING =
-  process.env.HFS_DB_CONNECTION_STRING ??
-  "postgres://postgres:admin@localhost:7000/postgres"
-
-export const DB_CLEAN = process.env.HFS_DB_CLEAN !== undefined
