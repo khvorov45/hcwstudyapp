b4ccb935b7e31484bdf627642f97afb165ec511a
Arseniy Khvorov <khvorov45@gmail.com>
2020-12-04 11:47:48 +1100

    have backend port be specified in arguments

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,7 +2,6 @@ import express, { Request, Response } from "express"
 import httpStatus from "http-status-codes"
 import pgp from "pg-promise"
 import yargs from "yargs"
-import { BACKEND_PORT } from "./config"
 import { dropSchema, init, isEmpty } from "./db"
 
 // Database connection
@@ -37,11 +36,13 @@ async function main() {
   const args = yargs(process.argv)
     .string("connectionString")
     .boolean("clean")
+    .number("backendPort")
     .default(
       "connectionString",
       "postgres://postgres:admin@localhost:7000/postgres"
     )
-    .default("clean", false).argv
+    .default("clean", false)
+    .default("backendPort", 7001).argv
 
   // Sort out the database connection
   let _db
@@ -62,8 +63,8 @@ async function main() {
     res.status(httpStatus.OK).send("Hello World!")
   })
 
-  app.listen(BACKEND_PORT, () => {
-    console.log(`server started on port ${BACKEND_PORT}`)
+  app.listen(args.backendPort, () => {
+    console.log(`server started on port ${args.backendPort}`)
   })
 }
 
