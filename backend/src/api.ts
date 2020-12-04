5b4fe3d34a1df1b35a4b17a56c2db77733ea301f
Arseniy Khvorov <khvorov45@gmail.com>
2020-12-04 12:08:09 +1100

    have the update path return no content

--- backend/src/api.ts
+++ backend/src/api.ts
@@ -1,9 +1,10 @@
 import { Router, Request, Response } from "express"
+import httpStatus from "http-status-codes"
 
 export function getRoutes() {
   const routes = Router()
   routes.get("/update", (req: Request, res: Response) => {
-    res.send("update")
+    res.status(httpStatus.NO_CONTENT).end()
   })
   return routes
 }
