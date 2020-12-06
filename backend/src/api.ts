ec531e62ca9b52bb4c3a2c9be53f9f8c87ded0bb
Arseniy Khvorov <khvorov45@gmail.com>
2020-12-07 10:11:50 +1100

    add POST /users endpoint

--- backend/src/api.ts
+++ backend/src/api.ts
@@ -1,5 +1,9 @@
 import { Router, Request, Response } from "express"
-import { getUsers, DB, getLastUpdate } from "./db"
+import randomString from "crypto-random-string"
+import SHA512 from "crypto-js/sha512"
+import { getUsers, DB, getLastUpdate, insertUser } from "./db"
+import { UserV } from "./data"
+import { decode } from "./io"
 
 export function getRoutes(db: DB) {
   const routes = Router()
@@ -9,5 +13,23 @@ export function getRoutes(db: DB) {
   routes.get("/users", async (req: Request, res: Response) => {
     res.json(await getUsers(db))
   })
+  routes.post("/users", async (req: Request, res: Response) => {
+    const token = generateToken()
+    const u = decode(UserV, {
+      email: req.body.email,
+      accessGroup: req.body.accessGroup,
+      tokenhash: hash(token),
+    })
+    await insertUser(db, u)
+    res.json(token)
+  })
   return routes
+}
+
+function generateToken(): string {
+  return randomString({ length: 40, type: "url-safe" })
+}
+
+function hash(s: string): string {
+  return SHA512(s).toString()
 }
