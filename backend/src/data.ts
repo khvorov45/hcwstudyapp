46fadc7f7c0e2c3c2400b09cdb980fac62c663b8
Arseniy Khvorov <khvorov45@gmail.com>
2020-12-07 12:06:22 +1100

    participant type

--- backend/src/data.ts
+++ backend/src/data.ts
@@ -1,4 +1,5 @@
 import * as t from "io-ts"
+import { date } from "io-ts-types"
 
 // https://github.com/gcanti/io-ts/blob/master/index.md#union-of-string-literals
 export const AccessGroupV = t.keyof({
@@ -14,6 +15,21 @@ export const UserV = t.type({
   tokenhash: t.string,
 })
 export type User = t.TypeOf<typeof UserV>
+
+export const ParticipantV = t.type({
+  redcapRecordId: t.string,
+  pid: t.string,
+  accessGroup: AccessGroupV,
+  site: t.string,
+  dateScreening: date,
+  email: t.string,
+  mobile: t.string,
+  addBleed: t.boolean,
+  dob: date,
+  gender: t.string,
+  withdrawn: t.string,
+})
+export type Participant = t.TypeOf<typeof ParticipantV>
 
 export const FullDataV = t.type({ user: UserV })
 export type FullData = t.TypeOf<typeof FullDataV>
