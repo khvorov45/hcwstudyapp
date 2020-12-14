5201706b0864617e6357ce4cc3da35cb6c643f34
Arseniy Khvorov <khvorov45@gmail.com>
2020-12-15 09:45:55 +1100

    style the nav bar

--- frontend/src/components/nav.tsx
+++ frontend/src/components/nav.tsx
@@ -1,13 +1,33 @@
-import { IconButton } from "@material-ui/core"
+import { createStyles, IconButton, makeStyles, Theme } from "@material-ui/core"
 import BrightnessMediumIcon from "@material-ui/icons/BrightnessMedium"
 import React from "react"
 
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    nav: {
+      overflow: "auto",
+      display: "flex",
+      justifyContent: "space-between",
+      alignItems: "center",
+      borderBottom: `1px solid ${theme.palette.divider}`,
+    },
+  })
+)
+
 export default function Nav({ togglePalette }: { togglePalette: () => void }) {
+  const classes = useStyles()
   return (
-    <div>
-      <IconButton onClick={(_) => togglePalette()}>
-        <BrightnessMediumIcon />
-      </IconButton>
+    <div className={classes.nav}>
+      {/* LEFT */}
+      <div></div>
+      {/* CENTER */}
+      <div></div>
+      {/* RIGHT */}
+      <div>
+        <IconButton onClick={(_) => togglePalette()}>
+          <BrightnessMediumIcon />
+        </IconButton>
+      </div>
     </div>
   )
 }
