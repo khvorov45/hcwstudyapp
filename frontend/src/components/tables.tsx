26acc7c75a491ae721afb7432a4518cb7ea25475
Arseniy Khvorov <khvorov45@gmail.com>
2020-12-16 10:11:58 +1100

    fetch participants for the contact table

--- frontend/src/components/tables.tsx
+++ frontend/src/components/tables.tsx
@@ -1,5 +1,10 @@
 import { Redirect, Route } from "react-router-dom"
+import StatusCodes from "http-status-codes"
+import * as t from "io-ts"
 import { SimpleNav } from "./nav"
+import { useAsync } from "react-async-hook"
+import { apiReq } from "../lib/api"
+import { ParticipantV } from "../lib/data"
 
 export default function Tables({ token }: { token: string | null }) {
   const tableNames = [
@@ -22,7 +27,9 @@ export default function Tables({ token }: { token: string | null }) {
       <Route path={"/tables"}>
         <Redirect to={tableLinks[0]} />
       </Route>
-      <Route path={tablePaths[0]}>Contact</Route>
+      <Route path={tablePaths[0]}>
+        <Contact token={token} />
+      </Route>
       <Route path={tablePaths[1]}>Baseline</Route>
       <Route path={tablePaths[2]}>Schedule</Route>
       <Route path={tablePaths[3]}>Weekly survey</Route>
@@ -30,4 +37,22 @@ export default function Tables({ token }: { token: string | null }) {
       <Route path={tablePaths[5]}>Summary</Route>
     </>
   )
+}
+
+function Contact({ token }: { token: string | null }) {
+  const participantsFetch = useAsync(
+    () =>
+      apiReq({
+        method: "GET",
+        url: "http://localhost:7001/participants",
+        token: token,
+        success: StatusCodes.OK,
+        failure: [StatusCodes.UNAUTHORIZED],
+        validator: t.array(ParticipantV),
+      }),
+    []
+  )
+  console.log(participantsFetch.result)
+  console.log(participantsFetch.error?.message)
+  return <>Contact</>
 }
