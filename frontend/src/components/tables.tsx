4cb26e765b1165692a377575e03dd8c57db6817f
Arseniy Khvorov <khvorov45@gmail.com>
2020-12-16 11:06:06 +1100

    fetch participants in parent

--- frontend/src/components/tables.tsx
+++ frontend/src/components/tables.tsx
@@ -31,34 +31,7 @@ export default function Tables({ token }: { token: string | null }) {
     name: n,
     link: tableLinks[i],
   }))
-  return (
-    <>
-      <SimpleNav links={tableNamedLinks} />
-      <Route path={"/tables"}>
-        <Redirect to={tableLinks[0]} />
-      </Route>
-      <Route path={tablePaths[0]}>
-        <Contact token={token} />
-      </Route>
-      <Route path={tablePaths[1]}>
-        <Baseline token={token} />
-      </Route>
-      <Route path={tablePaths[2]}>Schedule</Route>
-      <Route path={tablePaths[3]}>Weekly survey</Route>
-      <Route path={tablePaths[4]}>Weekly completion</Route>
-      <Route path={tablePaths[5]}>Summary</Route>
-    </>
-  )
-}
 
-function formatDate(d: Date | null | undefined): string {
-  if (!d) {
-    return ""
-  }
-  return d.toISOString().split("T")[0]
-}
-
-function Contact({ token }: { token: string | null }) {
   const participantsFetch = useAsync(
     () =>
       apiReq({
@@ -76,6 +49,34 @@ function Contact({ token }: { token: string | null }) {
     participantsFetch,
   ])
 
+  return (
+    <>
+      <SimpleNav links={tableNamedLinks} />
+      <Route path={"/tables"}>
+        <Redirect to={tableLinks[0]} />
+      </Route>
+      <Route path={tablePaths[0]}>
+        <Contact participants={participants} />
+      </Route>
+      <Route path={tablePaths[1]}>
+        <Baseline participants={participants} />
+      </Route>
+      <Route path={tablePaths[2]}>Schedule</Route>
+      <Route path={tablePaths[3]}>Weekly survey</Route>
+      <Route path={tablePaths[4]}>Weekly completion</Route>
+      <Route path={tablePaths[5]}>Summary</Route>
+    </>
+  )
+}
+
+function formatDate(d: Date | null | undefined): string {
+  if (!d) {
+    return ""
+  }
+  return d.toISOString().split("T")[0]
+}
+
+function Contact({ participants }: { participants: Participant[] }) {
   const columns = useMemo(() => {
     return [
       {
@@ -109,24 +110,7 @@ function Contact({ token }: { token: string | null }) {
   return <Table table={table} />
 }
 
-function Baseline({ token }: { token: string | null }) {
-  const participantsFetch = useAsync(
-    () =>
-      apiReq({
-        method: "GET",
-        url: "http://localhost:7001/participants",
-        token: token,
-        success: StatusCodes.OK,
-        failure: [StatusCodes.UNAUTHORIZED],
-        validator: t.array(ParticipantV),
-      }),
-    []
-  )
-
-  const participants = useMemo(() => participantsFetch.result ?? [], [
-    participantsFetch,
-  ])
-
+function Baseline({ participants }: { participants: Participant[] }) {
   const columns = useMemo(() => {
     return [
       {
