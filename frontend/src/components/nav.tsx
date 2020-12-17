a826a2320b1dccdaa0cbd0b641c0fe34be5580c6
Arseniy Khvorov <khvorov45@gmail.com>
2020-12-18 09:44:00 +1100

    funtional collapsible nav bar

--- frontend/src/components/nav.tsx
+++ frontend/src/components/nav.tsx
@@ -8,9 +8,12 @@ import {
 import BrightnessMediumIcon from "@material-ui/icons/BrightnessMedium"
 import People from "@material-ui/icons/People"
 import Home from "@material-ui/icons/Home"
-import React, { ReactNode } from "react"
+import ArrowForwardIosIcon from "@material-ui/icons/ArrowForwardIos"
+import ArrowBackIosIcon from "@material-ui/icons/ArrowBackIos"
+import React, { ReactNode, useState } from "react"
 import { Link } from "react-router-dom"
 import { User } from "../lib/data"
+import { useWindowSize } from "../lib/hooks"
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -95,19 +98,65 @@ function AuthOnly({
   return <>{children}</>
 }
 
+function getApproximateLinkWidth(linkText: string): number {
+  return 16 + linkText.length * 10
+}
+
+function getApproximateTextNavWidth(
+  links: { name: string; link: string }[]
+): number {
+  return links
+    .map((l) => l.name)
+    .map(getApproximateLinkWidth)
+    .reduce((s, a) => s + a, 0)
+}
+
+function findEnd(
+  links: { name: string; link: string }[],
+  desiredWidth: number,
+  start: number,
+  end: number
+): number {
+  if (
+    start === end ||
+    getApproximateTextNavWidth(links.slice(start, end)) < desiredWidth ||
+    desiredWidth === 0
+  ) {
+    return end
+  }
+  return findEnd(links, desiredWidth, start, end - 1)
+}
+
 export function SimpleNav({
   links,
 }: {
   links: { name: string; link: string }[]
 }) {
+  const windowSize = useWindowSize()
+  const [start, setStart] = useState(0)
+  const end = findEnd(links, windowSize.width, start, links.length)
   const classes = useStyles()
   return (
     <div className={classes.simpleNav}>
-      {links.map(({ name, link }) => (
+      {start > 0 ? (
+        <IconButton onClick={(_) => setStart(start - 1)}>
+          <ArrowBackIosIcon />
+        </IconButton>
+      ) : (
+        <></>
+      )}
+      {links.slice(start, end).map(({ name, link }) => (
         <Button key={name} component={Link} to={`${link}`}>
           {name}
         </Button>
       ))}
+      {end < links.length ? (
+        <IconButton onClick={(_) => setStart(start + 1)}>
+          <ArrowForwardIosIcon />
+        </IconButton>
+      ) : (
+        <></>
+      )}
     </div>
   )
 }
