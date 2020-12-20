732627388bd9453c1011fb06121be20b08ae2bb4
Arseniy Khvorov <khvorov45@gmail.com>
2020-12-21 09:04:21 +1100

    createToken function

--- backend/src/auth.ts
+++ backend/src/auth.ts
@@ -1,5 +1,19 @@
 import randomString from "crypto-random-string"
 import SHA512 from "crypto-js/sha512"
+import { Token } from "./data"
+
+function addDays(date: Date, days: number): Date {
+  date.setDate(date.getDate() + days)
+  return date
+}
+
+export function createToken(user: string, tokenDaysToLive: number): Token {
+  return {
+    user,
+    token: generateToken(),
+    expires: addDays(new Date(), tokenDaysToLive),
+  }
+}
 
 export function generateToken(): string {
   return randomString({ length: 40, type: "url-safe" })
