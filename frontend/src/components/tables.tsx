7bd29106cccf29c0448e360e67ec4747bc2c9a8a
Arseniy Khvorov <khvorov45@gmail.com>
2021-01-04 08:31:38 +1100

    cutom table for users

--- frontend/src/components/tables.tsx
+++ frontend/src/components/tables.tsx
@@ -365,7 +365,7 @@ function WeeklyCompletion({ weeklySurvey }: { weeklySurvey: WeeklySurvey[] }) {
   return <Table columns={columns} data={weeklyCompletion} />
 }
 
-export function Table<T extends object>({
+function Table<T extends object>({
   columns,
   data,
 }: {
--- frontend/src/components/users.tsx
+++ frontend/src/components/users.tsx
@@ -3,8 +3,41 @@ import { useAsync } from "react-async-hook"
 import * as t from "io-ts"
 import { apiReq } from "../lib/api"
 import { User, UserV } from "../lib/data"
-import { useMemo } from "react"
-import { Table } from "./tables"
+import React, { useMemo } from "react"
+import { useTable } from "react-table"
+import {
+  createStyles,
+  makeStyles,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Theme,
+} from "@material-ui/core"
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    tableContainer: {
+      maxWidth: 500,
+      margin: "auto",
+      "& th": {
+        borderRight: `1px solid ${theme.palette.divider}`,
+        borderBottom: `1px solid ${theme.palette.divider}`,
+      },
+      "& th:first-child": {
+        borderLeft: `1px solid ${theme.palette.divider}`,
+      },
+      "& td": {
+        borderBottom: "none",
+      },
+      "& tr:nth-child(even)": {
+        background: theme.palette.background.alt,
+      },
+    },
+  })
+)
 
 export default function Users({ token }: { token?: string }) {
   const usersFetch = useAsync(
@@ -35,5 +68,40 @@ export default function Users({ token }: { token?: string }) {
     ]
   }, [])
 
-  return <Table data={users} columns={columns} />
+  const table = useTable<User>({
+    columns: columns,
+    data: users,
+  })
+  const classes = useStyles()
+  return (
+    <TableContainer className={classes.tableContainer}>
+      <Table {...table.getTableProps()}>
+        <TableHead>
+          <TableRow>
+            {table.headers.map((c) => (
+              <TableCell {...c.getHeaderProps()}>
+                {c.render("Header")}
+              </TableCell>
+            ))}
+          </TableRow>
+        </TableHead>
+        <TableBody {...table.getTableBodyProps}>
+          {table.rows.map((row, i) => {
+            table.prepareRow(row)
+            return (
+              <TableRow {...row.getRowProps()}>
+                {row.cells.map((cell) => {
+                  return (
+                    <TableCell {...cell.getCellProps()}>
+                      {cell.render("Cell")}
+                    </TableCell>
+                  )
+                })}
+              </TableRow>
+            )
+          })}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  )
 }
