fc4d95fd0ddaee7d623ed195731ae8426a4196c3
Arseniy Khvorov <khvorov45@gmail.com>
2021-01-08 07:19:56 +1100

    move index css into mui overrides

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -105,6 +105,32 @@ export default function App() {
     localStorage.setItem("theme", newPalette)
   }
   const theme = createMuiTheme({
+    overrides: {
+      MuiCssBaseline: {
+        "@global": {
+          "html, html *": {
+            "scrollbar-color":
+              "rgb(150, 150, 150) " +
+              (paletteType === "dark" ? "rgb(0, 0, 0)" : "rgb(255, 255, 255)"),
+          },
+          "::-webkit-scrollbar-thumb:hover": {
+            background:
+              paletteType === "dark" ? "rgb(255, 255, 255)" : "rgb(0, 0, 0)",
+          },
+          "::-webkit-scrollbar-thumb": {
+            background: "rgb(150, 150, 150)",
+          },
+          "::-webkit-scrollbar-track, ::-webkit-scrollbar-corner": {
+            background:
+              paletteType === "dark" ? "rgb(0, 0, 0)" : "rgb(255, 255, 255)",
+          },
+          "::-webkit-scrollbar": {
+            width: 10,
+            height: 10,
+          },
+        },
+      },
+    },
     palette: {
       type: paletteType,
       background: {
