6d36f4059d37b9b60a770d2254dc3a0e73c52f97
Arseniy Khvorov <khvorov45@gmail.com>
2021-01-11 07:08:06 +1100

    remove the old next stuff

--- scripts/reset-default-db.ts
+++
@@ -1,9 +0,0 @@
-import { Postgres } from '../lib/db'
-
-test('reset-default-db', async () => {
-  // Default initialisation (check that it doesn't fail but don't modify)
-  const db = new Postgres()
-  await db.update(true)
-  expect(await db.isEmpty()).toBe(false)
-  await db.end()
-}, 25000)
--- scripts/reset-participants.ts
+++
@@ -1,8 +0,0 @@
-import { Postgres } from '../lib/db'
-
-test('reset-participants', async () => {
-  const db = new Postgres()
-  await db.resetAllParticipantTables()
-  expect(await db.isEmpty()).toBe(false)
-  await db.end()
-}, 25000)
