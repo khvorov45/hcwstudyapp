43c4749cd3e9d95f8839428e5f9d619a2ca179e5
Arseniy Khvorov <khvorov45@gmail.com>
2021-01-11 11:46:55 +1100

    use window host when api starts with a forward slash

--- frontend/src/components/api-spec.tsx
+++ frontend/src/components/api-spec.tsx
@@ -154,7 +154,9 @@ function RCode() {
 # ${tableNames.join(", ")}
 pull_table <- function(table_name) {
   httr::GET(
-    paste0("${API_ROOT}/", table_name),
+    paste0("${
+      API_ROOT.startsWith("/") ? window.location.host + "/" : ""
+    }${API_ROOT}/", table_name),
     # Replace token with the actual token
     httr::add_headers(Authorization = "Bearer token")
   ) %>%
