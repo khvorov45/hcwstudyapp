8c5c4e14459b06d7752b4a947f067b866c2d45b0
Arseniy Khvorov <khvorov45@gmail.com>
2021-01-20 10:32:41 +1100

    change width and height of the serology plot

--- frontend/src/components/plot.tsx
+++ frontend/src/components/plot.tsx
@@ -150,8 +150,8 @@ function SerologyPlots({
       </div>
       <div>
         <LineChart
-          width={450}
-          height={250}
+          width={500}
+          height={300}
           data={serologyWide}
           margin={{ top: 20, right: 20, bottom: 20, left: 20 }}
         >
