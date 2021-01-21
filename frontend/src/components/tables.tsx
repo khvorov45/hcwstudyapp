6047170da1e6797eab96ae6efe85644965fbd471
Arseniy Khvorov <khvorov45@gmail.com>
2021-01-22 10:02:24 +1100

    center text in tables

--- frontend/src/components/tables.tsx
+++ frontend/src/components/tables.tsx
@@ -76,6 +76,7 @@ const useStyles = makeStyles((theme: Theme) =>
       },
       "& .th, & .td": {
         padding: "0.5rem",
+        textAlign: "center",
       },
       "& .header-content": {
         display: "flex",
