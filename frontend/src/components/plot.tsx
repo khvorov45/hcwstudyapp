951c5223c97543dc13badd37e9fdb09309214ffa
Arseniy Khvorov <khvorov45@gmail.com>
2021-02-02 07:51:26 +1100

    change x angle for primary axis

--- frontend/src/components/plot.tsx
+++ frontend/src/components/plot.tsx
@@ -290,7 +290,7 @@ function SerologyPlots({
           yRange={[5, 10240]}
           yTicks={[5, 10, 20, 40, 80, 160, 320, 640, 1280, 2560, 5120, 10240]}
           yLab={selectedPid ? "Titre" : "GMT (95% CI)"}
-          xAngle={-45}
+          xAngle={0}
           xLab="Vax"
           xTickDy={10}
           x2Lab="Day"
