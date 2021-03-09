6cf58503ffcffbd14bc747cea191af37fd6dc77e
Arseniy Khvorov <khvorov45@gmail.com>
2021-03-09 14:17:07 +1100

    restyle navbar

--- frontend-sapper/src/components/Nav.svelte
+++ frontend-sapper/src/components/Nav.svelte
@@ -1,15 +1,30 @@
 <script lang="ts">
   import { loginStatus } from "../lib/state"
+  import Link from "./Link.svelte"
 
   export let segment: string | undefined
 </script>
 
 <nav>
-  <a href="/" class:active={segment === undefined}>Home</a>
+  <div class:active={segment === undefined}><Link href="/">Home</Link></div>
 
-  <a href="email" class:active={segment === "email"}>Email</a>
+  <div class:active={segment === "email"}><Link href="email">Email</Link></div>
 
   {#if $loginStatus.status === "success"}
-    <a href="protected" class:active={segment === "protected"}>Protected</a>
+    <div class:active={segment === "protected"}>
+      <Link href="protected">Protected</Link>
+    </div>
   {/if}
 </nav>
+
+<style>
+  nav {
+    display: flex;
+  }
+  nav > * {
+    margin-left: 5px;
+  }
+  nav > *:last-child {
+    margin-right: 5px;
+  }
+</style>
