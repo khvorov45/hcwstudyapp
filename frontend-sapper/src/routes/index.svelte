1b7e803d1ede4703780df2077b2609b03cd110bf
Arseniy Khvorov <khvorov45@gmail.com>
2021-03-10 11:39:41 +1100

    add homepage content

--- frontend-sapper/src/routes/index.svelte
+++ frontend-sapper/src/routes/index.svelte
@@ -1,1 +1,9 @@
-<h1>Home</h1>
+<script lang="ts">
+  import Link from "../components/Link.svelte"
+</script>
+
+<h1>Reports for HCW flu study</h1>
+
+<p>Main site is <Link href="https://hcwflustudy.com">here</Link>.</p>
+
+<p>Site map is <Link href="search">here</Link>.</p>
