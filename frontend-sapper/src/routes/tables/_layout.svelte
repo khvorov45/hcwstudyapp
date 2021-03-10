c625113eaefc5b7032ad3809893291dd53727cb3
Arseniy Khvorov <khvorov45@gmail.com>
2021-03-10 12:37:52 +1100

    add contact table stub and layout

---
+++ frontend-sapper/src/routes/tables/_layout.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  export let segment: string | undefined
+</script>
+
+{#if segment === undefined}
+  <slot />
+{:else}
+  Subnavbar
+  <slot />
+{/if}
--- frontend-sapper/src/routes/tables/index.svelte
+++ frontend-sapper/src/routes/tables/index.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
+  import Link from "../../components/Link.svelte"
   import TextContainer from "../../components/TextContainer.svelte"
 </script>
 
 <TextContainer>
   <h1>Tables</h1>
+  <ul><li><Link href="tables/contact">Contact</Link></li></ul>
 </TextContainer>
