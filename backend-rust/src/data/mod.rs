f08e15cb57db65c99998cf0d606fc1ad64651a6f
Arseniy Khvorov <khvorov45@gmail.com>
2021-03-11 14:19:13 +1100

    take impls out to mod

--- backend-rust/src/data/mod.rs
+++ backend-rust/src/data/mod.rs
@@ -1,2 +1,50 @@
+use crate::db::ToCurrent;
+
 pub mod current;
 pub mod previous;
+
+impl ToCurrent<current::UserKind> for previous::UserKind {
+    fn to_current(&self) -> current::UserKind {
+        use previous::UserKind::*;
+        match self {
+            Redcap => current::UserKind::Redcap,
+            Manual => current::UserKind::Manual,
+        }
+    }
+}
+
+impl ToCurrent<current::Site> for previous::Site {
+    fn to_current(&self) -> current::Site {
+        use previous::Site::*;
+        match self {
+            Melbourne => current::Site::Melbourne,
+            Sydney => current::Site::Sydney,
+            Adelaide => current::Site::Adelaide,
+            Brisbane => current::Site::Brisbane,
+            Newcastle => current::Site::Newcastle,
+            Perth => current::Site::Perth,
+        }
+    }
+}
+
+impl ToCurrent<current::AccessGroup> for previous::AccessGroup {
+    fn to_current(&self) -> current::AccessGroup {
+        use previous::AccessGroup::*;
+        match self {
+            Site(site) => current::AccessGroup::Site(site.to_current()),
+            Unrestricted => current::AccessGroup::Unrestricted,
+            Admin => current::AccessGroup::Admin,
+        }
+    }
+}
+
+impl ToCurrent<current::User> for previous::User {
+    fn to_current(&self) -> current::User {
+        current::User {
+            email: self.email.clone(),
+            access_group: self.access_group.to_current(),
+            kind: self.kind.to_current(),
+            deidentified_export: self.deidentified_export,
+        }
+    }
+}
--- backend-rust/src/data/previous.rs
+++ backend-rust/src/data/previous.rs
@@ -1,5 +1,3 @@
-use crate::data::current;
-use crate::db::ToCurrent;
 use serde_derive::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Clone)]
@@ -31,52 +29,4 @@ pub struct User {
     pub access_group: AccessGroup,
     pub kind: UserKind,
     pub deidentified_export: bool,
-}
-
-// ================================================================================================
-
-impl ToCurrent<current::UserKind> for UserKind {
-    fn to_current(&self) -> current::UserKind {
-        use UserKind::*;
-        match self {
-            Redcap => current::UserKind::Redcap,
-            Manual => current::UserKind::Manual,
-        }
-    }
-}
-
-impl ToCurrent<current::Site> for Site {
-    fn to_current(&self) -> current::Site {
-        use Site::*;
-        match self {
-            Melbourne => current::Site::Melbourne,
-            Sydney => current::Site::Sydney,
-            Adelaide => current::Site::Adelaide,
-            Brisbane => current::Site::Brisbane,
-            Newcastle => current::Site::Newcastle,
-            Perth => current::Site::Perth,
-        }
-    }
-}
-
-impl ToCurrent<current::AccessGroup> for AccessGroup {
-    fn to_current(&self) -> current::AccessGroup {
-        use AccessGroup::*;
-        match self {
-            Site(site) => current::AccessGroup::Site(site.to_current()),
-            Unrestricted => current::AccessGroup::Unrestricted,
-            Admin => current::AccessGroup::Admin,
-        }
-    }
-}
-
-impl ToCurrent<current::User> for User {
-    fn to_current(&self) -> current::User {
-        current::User {
-            email: self.email.clone(),
-            access_group: self.access_group.to_current(),
-            kind: self.kind.to_current(),
-            deidentified_export: self.deidentified_export,
-        }
-    }
 }
