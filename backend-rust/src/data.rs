ce6d59e1594bf7487c1bd1050b16ddddb4188b6c
Arseniy Khvorov <khvorov45@gmail.com>
2021-03-11 12:45:15 +1100

    implement db versioning

--- backend-rust/src/data.rs
+++ backend-rust/src/data.rs
@@ -1,6 +1,6 @@
 use serde_derive::{Deserialize, Serialize};
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Clone)]
 pub enum Site {
     Melbourne,
     Sydney,
@@ -10,23 +10,29 @@ pub enum Site {
     Perth,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Clone)]
 pub enum AccessGroup {
     Site(Site),
     Unrestricted,
     Admin,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Clone)]
 pub enum UserKind {
     Redcap,
     Manual,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Clone)]
 pub struct User {
     email: String,
     access_group: AccessGroup,
     kind: UserKind,
     deidentified_export: bool,
+}
+
+impl crate::db::ToCurrent<User> for User {
+    fn to_current(&self) -> User {
+        self.clone()
+    }
 }
--- backend-rust/src/db.rs
+++ backend-rust/src/db.rs
@@ -7,7 +7,7 @@ use std::path::{Path, PathBuf};
 
 pub struct Db {
     pub dirs: DbDirs,
-    pub users: Table<data::User>,
+    pub users: Table<data::User, data::User>,
 }
 
 pub struct DbDirs {
@@ -17,6 +17,7 @@ pub struct DbDirs {
     pub current: PathBuf,
 }
 
+#[derive(PartialEq)]
 pub enum DbDirsInitState {
     /// No directories present at init
     None,
@@ -26,24 +27,52 @@ pub enum DbDirsInitState {
     Current,
 }
 
-pub struct Table<T: Serialize + DeserializeOwned> {
+pub struct Table<P, C> {
     pub name: String,
+    pub previous: TableData<P>,
+    pub current: TableData<C>,
+}
+
+pub struct TableData<T> {
     pub path: PathBuf,
     pub data: Vec<T>,
 }
 
+#[derive(Debug)]
+pub enum Version {
+    Previous,
+    Current,
+}
+
+// ================================================================================================
+
 impl Db {
+    /// Will read in the data depending on the initial state of the directories
     pub fn new(dir: PathBuf) -> Result<Self> {
-        let mut db = Self {
-            dirs: DbDirs::new(dir.as_path())?,
-            users: Table::new("User", dir.as_path())?,
-        };
-        db.read_from_disk()?;
+        log::debug!("initializing db at root directory {:?}", dir);
+
+        let dirs = DbDirs::new(dir.as_path())?;
+        let users = Table::new("User", &dirs)?;
+
+        let mut db = Self { dirs, users };
+
+        if db.dirs.init_state == DbDirsInitState::Previous {
+            match db.read(Version::Previous) {
+                Ok(()) => {}
+                Err(e) => {
+                    fs::remove_dir_all(db.dirs.current.as_path())?;
+                    return Err(e);
+                }
+            };
+        } else if db.dirs.init_state == DbDirsInitState::Current {
+            db.read(Version::Current)?;
+        }
+
         Ok(db)
     }
-    pub fn read_from_disk(&mut self) -> Result<()> {
-        log::debug!("reading db from disk");
-        self.users.read()?;
+    pub fn read(&mut self, version: Version) -> Result<()> {
+        log::debug!("reading db version {:?} from disk", version);
+        self.users.read(version)?;
         Ok(())
     }
     pub fn write_to_disk(&self) -> Result<()> {
@@ -53,45 +82,54 @@ impl Db {
     }
 }
 
-impl<T: Serialize + DeserializeOwned> Table<T> {
-    pub fn new<P: AsRef<Path>>(name: &str, root_dir: P) -> Result<Self> {
-        let path = root_dir
-            .as_ref()
-            .to_path_buf()
-            .join(format!("{}.json", name));
-        if !path.is_file() {
-            fs::write(path.as_path(), "[]")?;
+impl<P: Serialize + DeserializeOwned + ToCurrent<C>, C: Serialize + DeserializeOwned> Table<P, C> {
+    /// Creates table with empty data
+    pub fn new(name: &str, dirs: &DbDirs) -> Result<Self> {
+        let file_name = format!("{}.json", name);
+        let previous = dirs.previous.join(file_name.as_str());
+        let current = dirs.current.join(file_name);
+        if dirs.init_state == DbDirsInitState::Previous && !previous.is_file() {
+            fs::write(previous.as_path(), "[]")?;
+        }
+        if !current.is_file() {
+            fs::write(current.as_path(), "[]")?;
         }
         Ok(Self {
             name: name.to_string(),
-            path,
-            data: Vec::new(),
+            previous: TableData::new(previous),
+            current: TableData::new(current),
         })
     }
-    pub fn read(&mut self) -> Result<()> {
-        let file = File::open(self.path.as_path()).context(format!(
-            "table {} file {:?} failed to open",
-            self.name, self.path
-        ))?;
-        let reader = BufReader::new(file);
-        let data = serde_json::from_reader(reader).context(format!(
-            "table {} file {:?} failed to parse",
-            self.name, self.path
-        ))?;
-        self.data = data;
+    pub fn read(&mut self, version: Version) -> Result<()> {
+        match version {
+            Version::Previous => {
+                self.previous.read()?;
+                self.convert();
+            }
+            Version::Current => {
+                self.current.read()?;
+            }
+        }
         Ok(())
     }
     pub fn write(&self) -> Result<()> {
         fs::write(
-            self.path.as_path(),
-            serde_json::to_string(&self.data)
+            self.current.path.as_path(),
+            serde_json::to_string(&self.current.data)
                 .context(format!("table {} failed to serialize", self.name))?,
         )
         .context(format!(
             "table {} file {:?} failed to write",
-            self.name, self.path
+            self.name, self.current.path
         ))?;
         Ok(())
+    }
+    pub fn convert(&mut self) {
+        let mut converted = Vec::with_capacity(self.previous.data.len());
+        for row in &self.previous.data {
+            converted.push(row.to_current());
+        }
+        self.current.data = converted;
     }
 }
 
@@ -122,4 +160,30 @@ impl DbDirs {
             init_state,
         })
     }
+}
+
+impl<T: Serialize + DeserializeOwned> TableData<T> {
+    /// Empty data
+    pub fn new(path: PathBuf) -> Self {
+        Self {
+            path,
+            data: Vec::new(),
+        }
+    }
+    pub fn read(&mut self) -> Result<()> {
+        let file = File::open(self.path.as_path())
+            .context(format!("file {:?} failed to open", self.path,))?;
+
+        let reader = BufReader::new(file);
+        let data = serde_json::from_reader(reader)
+            .context(format!("file {:?} failed to parse", self.path))?;
+
+        self.data = data;
+
+        Ok(())
+    }
+}
+
+pub trait ToCurrent<C> {
+    fn to_current(&self) -> C;
 }
