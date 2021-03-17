024cdffa5aa15c95759653b4f6a89de37dd643ad
Arseniy Khvorov <khvorov45@gmail.com>
2021-03-18 07:40:38 +1100

    simplify createLocalStore

--- frontend-sapper/src/lib/state.ts
+++ frontend-sapper/src/lib/state.ts
@@ -1,39 +1,26 @@
 import { writable } from "svelte/store"
 
-function createLocalStore<T>(
-  key: string,
-  startValue: T | null,
-  toString: (x: T) => string,
-  fromString: (x: string) => T
-) {
+function createLocalStore<T>(key: string, startValue: T | null) {
   const { subscribe, set, update } = writable(startValue)
-  let localStorageInit = false
 
   return {
     subscribe,
     set,
     update,
-    localStorageInit,
     useLocalStorage: () => {
       const json = localStorage.getItem(key)
       if (json) {
-        set(fromString(json))
+        set(JSON.parse(json))
       }
 
       subscribe((current) => {
-        localStorage.setItem(key, toString(current))
+        localStorage.setItem(key, JSON.stringify(current))
       })
-      localStorageInit = true
     },
   }
 }
 
-export const token = createLocalStore(
-  "token",
-  null,
-  (x) => x,
-  (x) => x
-)
+export const token = createLocalStore("token", null)
 
 export const loginStatus = writable({
   status: "not-requested",
