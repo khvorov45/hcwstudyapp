e2d70cb86ea94c7d50ecbc40cba7e1bccb41ab38
Arseniy Khvorov <khvorov45@gmail.com>
2021-03-19 10:10:43 +1100

    add link to email for non-logged in

--- frontend-sapper/src/routes/index.svelte
+++ frontend-sapper/src/routes/index.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import Link from "../components/Link.svelte"
   import TextContainer from "../components/TextContainer.svelte"
+  import { loginStatus } from "../lib/state"
 </script>
 
 <TextContainer>
@@ -9,4 +10,8 @@
   <p>Main site is <Link href="https://hcwflustudy.com">here</Link>.</p>
 
   <p>Site map is <Link href="search">here</Link>.</p>
+
+  {#if $loginStatus.status === "error"}
+    <p>Access link can be obtained <Link href="email">here</Link></p>
+  {/if}
 </TextContainer>
