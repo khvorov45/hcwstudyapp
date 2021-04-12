3f8e7b24b53b46dd7007aaccbaccba0b72404d06
Arseniy Khvorov <khvorov45@gmail.com>
2021-04-12 13:44:28 +1000

    links to tables on the index page

--- frontend-sveltekit/src/routes/tables/$layout.svelte
+++ frontend-sveltekit/src/routes/tables/$layout.svelte
@@ -2,6 +2,8 @@
   import { page } from "$app/stores"
   import Subnav from "$lib/components/Subnav.svelte"
   import type { SubnavLink } from "$lib/util"
+  import Link from "$lib/components/Link.svelte"
+  import TextContainer from "$lib/components/TextContainer.svelte"
 
   const tableLinks: SubnavLink[] = [
     {
@@ -18,8 +20,22 @@
 </script>
 
 {#if $page.path === "/tables"}
-  <slot />
+  <TextContainer>
+    <h1>Tables</h1>
+    <ul>
+      {#each tableLinks as link}
+        <li><Link href={link.link}>{link.title}</Link></li>
+      {/each}
+    </ul>
+  </TextContainer>
 {:else}
   <Subnav links={tableLinks} />
   <slot />
 {/if}
+
+<style>
+  li {
+    font-size: 16px;
+    margin-bottom: 10px;
+  }
+</style>
--- frontend-sveltekit/src/routes/tables/index.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-  import Link from "$lib/components/Link.svelte"
-  import TextContainer from "$lib/components/TextContainer.svelte"
-</script>
-
-<TextContainer>
-  <h1>Tables</h1>
-  <ul><li><Link href="/tables/contact">Contact</Link></li></ul>
-</TextContainer>
