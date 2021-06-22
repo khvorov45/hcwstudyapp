d82243298118ea989cbe200b1b36cddddd16b87a
Arseniy Khvorov <khvorov45@gmail.com>
2021-06-22 10:20:19 +1000

    add summary page

---
+++ frontend-sveltekit/src/routes/tables/summary.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import TextContainer from "$lib/components/TextContainer.svelte"
+  import Link from "$lib/components/Link.svelte"
+</script>
+
+<TextContainer>
+  <h3>Summary is now split into two tables:</h3>
+  <ul>
+    <li><Link href="/tables/demographics">Demographics</Link></li>
+    <li><Link href="/tables/summary-titre">Titres</Link></li>
+  </ul>
+</TextContainer>
+
+<style>
+  li {
+    font-size: 16px;
+    margin-bottom: 10px;
+  }
+</style>
